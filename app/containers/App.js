78dbde1b4cc22be1d38dc1ed88e3407b760a2178
qitmac000068 <qitmac000068@qitmac000068deMacBook-Pro.local>
2017-07-07 11:39:18 +0800

    first commit

---
+++ app/containers/App.js
@@ -0,0 +1,3 @@
+/**
+ * Created by qitmac000068 on 2017/7/7.
+ */
---
+++ app/index.js
@@ -0,0 +1,14 @@
+/**
+ * Created by qitmac000068 on 2017/7/7.
+ */
+import React from 'react';
+import ReactDom from 'react-dom';
+import {Route,IndexRoute,browserHistory,Router} from 'react-router';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+
+import './index.scss';
+
+ReactDom.render(
+
+)
